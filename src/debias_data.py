e2e46eeadfc09829a16b1519bdf9019e0bc0db73
Viktor torp Thomsen <vigis.thomsen@gmail.com>
2022-05-23 09:52:17 +0200

    Model evaluation system and debias data function

---
+++ src/debias_data.py
@@ -0,0 +1,72 @@
+import numpy as np
+import scipy
+
+def reproject_features(data, protected_cols, nonprotected_cols):
+    """
+    generate a fair representation of nonprotected columns which are independent from any columns in protected_cols
+    data: a data frame
+    protected_cols: list of strings, the protected columns
+    nonprotected_col: string, all other data columns 
+    
+    NOTE: this function assumes the data is already centered 
+    """
+    # make a copy of data
+    df = data.copy()
+    # Protected features
+    protect = df[protected_cols].values
+    # extract data about nonprotected columns
+    debiased_nonprotect = df[nonprotected_cols].values
+    # crease an orthonormal basis
+    base_protect = scipy.linalg.orth(protect)
+    for j in range(debiased_nonprotect.shape[1]):
+        debiased_nonprotect[:,j] -= base_protect @ base_protect.T @ debiased_nonprotect[:,j]
+    return debiased_nonprotect
+
+def reproject_features_w_regul(data, protected_cols, nonprotected_cols, lambda_):
+    """
+    generate a fair representation of nonprotected columns which are independent from any columns in protected_cols
+    dat_: a data frame
+    protected_cols: list of strings, the protected columns
+    nonprotected_col: string, all other data columns 
+    lambda_: float number between 0 and 1, 0 means totally fair; 1 means same as raw data
+    """
+    
+    # run the normal reproject_features function
+    r = reproject_features(data, protected_cols, nonprotected_cols)
+    
+    # extract data about nonprotected variables
+    nonprotect = data[nonprotected_cols].values
+    # standardize columns
+
+    return r + lambda_*(nonprotect - r)
+
+
+def debais_data(protected_cols, nonprotected_cols, lambda_=0):
+    """
+    Debias data, data pipeline
+    
+    Arguments
+    ---------
+    protected_cols: list of strings, 
+        the protected columns
+    
+    nonprotected_col: String, 
+        all other data columns 
+    
+    Returns
+    -------
+    Function(X_train, X_test) => X_train_r, X_test_r
+        A fucntion which takes X_train, X_test and returns the reprojected versions of X_train, X_test
+    
+    """
+    def call(X_train, X_test):
+        # Standard scale the data data
+        _mean = np.mean(X_train, axis=0)
+        _std = np.std(X_train, ddof=1, axis=0)
+        X_train_scaled = (X_train - _mean)/_std
+        X_test_scaled = (X_test - _mean)/_std
+        # reproject features
+        X_train_r = reproject_features_w_regul(X_train_scaled, protected_cols, nonprotected_cols, lambda_)
+        X_test_r = reproject_features_w_regul(X_test_scaled, protected_cols, nonprotected_cols, lambda_)
+        return X_train_r, X_test_r
+    return call
---
+++ src/model_selection.py
@@ -0,0 +1,58 @@
+import numpy as np
+import pandas as pd
+
+
+def test_model(model, X, y, kfold, scaler=None, model_params={}):
+    """
+    Test models using some cross validation method and with a scaling method.
+
+    Parameters
+    ----------
+    model : sklearn model object. 
+        This object must not be instansiated
+
+    X : pandas.Dataframe
+        df containing all the features, which the model should be trained on
+
+    y : np.array
+        An array on the taget values
+
+    kfold : sklearn.model_selection method e.g. KFold
+        This object must be initialised. See example
+
+    scaler : Function, default=None
+        Data processing pipeline of the form 
+        (X_train, X_test) => X_train_processed, X_test_processed
+        If None, then are the data not processed
+
+    model_params : Dict, default=Dict
+        A dictionary containing model parameters
+
+
+    Returns
+    -------
+    numpy.array
+        An array containing the predictions
+
+
+    Example
+    -------
+    X, y = df.drop(columns=['credit_risk']), df['credit_risk'].values
+    kfold = StratifiedKFold(n_splits=5, shuffle=True, random_state=1)
+    process_data = scale_data()
+    model = LogisticRegression
+    preds = test_model(model, X, y, kfold, process_data)
+    """
+
+    preds = np.zeros(len(y))
+
+    for train_index, test_index in kfold.split(X, y):
+        X_train, X_test = X.iloc[train_index], X.iloc[test_index]
+        y_train, y_test = y[train_index], y[test_index]
+        if scaler:
+            X_train, X_test = scaler(X_train, X_test)
+
+        tmp_model = model(**model_params)
+        tmp_model = tmp_model.fit(X_train, y_train)
+        preds[test_index] = tmp_model.predict(X_test)
+    return preds
